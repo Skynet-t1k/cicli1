1cdf4fec8783af0360f95a96ad5063cc39ce262c
Giuseppe Cattano <giuseppe.cattano@gmail.com>
2024-04-25 12:33:24 +0200

    completato esercizio cicli1

---
+++ main.js
@@ -0,0 +1,9 @@
+// Scrivi un programma che dato un numero, let num = 2, stampi la rispettiva tabellina corrispondente.
+
+let num = 7;
+
+let i = 1;
+while (i<=10) {
+    console.log(num*i);
+    i++;
+}
